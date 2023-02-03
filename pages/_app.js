7c41b1afb12091dbf286ef87dd23a26ebcdecdfd
Claire Wang <clara32356@gmail.com>
2023-02-03 16:29:00 -0500

    update resume

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
-	<Analytics />
+	    <Analytics />
     </ThemeProvider>
   )
 }
