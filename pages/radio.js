ba36a9565a75c729ea6e4337e6120c7db35b0573
clairebookworm <clara32356@gmail.com>
2025-09-06 17:21:31 -0400

    radio

---
+++ pages/radio.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Radio() {
+	return (
+		<>
+			<Head>
+				<title>radio</title>
+			</Head>
+			<main>
+				<h1>death car for qt</h1>
+				<p>Welcome to the radio page. under construction.</p>
+			</main>
+		</>
+	);
+}
