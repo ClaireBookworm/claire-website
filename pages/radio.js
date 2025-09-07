410b6d4c63e2bf5cfd215ec87a6d114a59d2477b
clairebookworm <clara32356@gmail.com>
2025-09-06 20:18:34 -0400

    radio page

--- pages/radio.js
+++ pages/radio.js
@@ -1,15 +1,16 @@
 import Head from 'next/head';
+import Layout from "../components/layout";
 
 export default function Radio() {
 	return (
-		<>
+		<Layout>
 			<Head>
 				<title>radio</title>
 			</Head>
 			<main>
-				<h1>death car for qt</h1>
-				<p>Welcome to the radio page. under construction.</p>
+				<h1 className="heading">death car for qt</h1>
+				<p className="gallerySubhead">Welcome to the radio page. under construction. :)</p>
 			</main>
-		</>
+		</Layout>
 	);
 }
