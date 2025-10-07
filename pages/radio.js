4d195100c59f0e6bb80d91634f25352bc79e0bd1
clairebookworm <clara32356@gmail.com>
2025-10-07 01:44:51 -0400

    style and week 2 ntoes

--- pages/radio.js
+++ pages/radio.js
@@ -9,7 +9,7 @@ export default function Radio() {
 			</Head>
 			<main>
 				<h1 className="heading">death car for qt</h1>
-				<section className="selfbio mt-8 md:mt-16 w-2/3 lg:w-1/2 md:text-xl">
+				<section className="selfbio mt-8 md:mt-16 w-2/3 lg:w-1/2 md:text-xl" style={{lineHeight: '1.2rem'}}>
 					<span style={{marginBottom: "1rem", display: "block"}}>
 						<p>Welcome to the radio page!! Death Car for QT was inspired by really good production and how many shared sampling and styles there are between genres and artists. I'm a huge fan of Jack Antonoff and I think, in general, there's a lot of skill to pop music that's ignored. Beyond that, I really love jazz, electronic music, r&b, alternative indie, punk/psychdelic rock... and want to play them! <br /><br />Every week, I'll have a "connections" with three songs that have something in common. You can also find the tracklist from my <a className="landing-link" href="https://track-blaster.com/wmbr/?startdt=1984-06-28&enddt=2030-09-23&sort=desc&program=587">WMBR track-blaster</a>. <strong>Listen at 88.1 FM WMBR in Boston or at <a className="landing-link" style={{fontStyle: 'italic'}} href="https://wmbr.org/">wmbr.org</a> at Monday nights (12-1AM EST).</strong></p>
 					</span>
@@ -32,10 +32,14 @@ export default function Radio() {
 
 					<br /><br />
 
+					<p>Honestly, I kind of forgot to talk about the connections this week on the actual radio segment, but it just so conveniently happens that there are 3 jack antonoff produced songs in this tracklist! The Whole of the Moon (Live at Radio City Hall) is a cover by Bleachers of the original The Waterboys track and might be one of my favorite live tracks ever. Then, Comeback by Carly Rae Jepson & Bleachers is what I consider to be the perfect culmination of "real" pop: catchy, joyful, clean blend of production and lyrics, and doesn't try too hard what it isn't. That song and I'm In Love With You by the 1975 are produced by Jack Antonoff, but have very different interpretations of what 'pop' means. Finally, just on a personal anecdote, Turnstile and Remi Wolf performed at the most recent (2024) <a href="https://theallycoalition.org/">The Ally Coalition</a> Talent Show in New York as they are good friends with the foundation, founded by Jack and Rachel Antonoff.</p>
+
+					<br /><br />
+
 					<p style={{fontWeight: 'bold', fontSize: '1.8rem'}}>Week 3</p>
 					<br /><br />
 					<iframe data-testid="embed-iframe" style={{"border-radius": "12px"}} src="https://open.spotify.com/embed/playlist/7IJLBQV3tcVn0yZilTSccl?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-					<p><a className="landing-link" href="https://track-blaster.com/wmbr/playlist.php?id=58050">Track Blaster Link / Recording</a></p>
+					<p style={{paddingTop: '5px'}}><a className="landing-link" href="https://track-blaster.com/wmbr/playlist.php?id=58050">link to recording + track blaster list</a></p>
 
 					<br /><br />
 
